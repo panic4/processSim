36b18dfebe53713210fcb1d506233bb965b8d6c7
j20 <j7891@protonmail.com>
2022-12-20 19:26:57 -0500

    added import, importlist, plist, create, spawn, exec command functionality; added example.py

--- processSim.py
+++ processSim.py
@@ -1,38 +1,64 @@
-from importlib import import_module
-import multiprocessing as mp
-import cmd
-mp.set_start_method('spawn',force=True) #force processes to use spawn
+from multiprocessing import Process,set_start_method
+from cmd import Cmd
 
-importlist=['import_module from importlib','multiprocessing as mp','cmd'] #initialize list of imports with default imported modules
-
-class t(cmd.Cmd):
+class t(Cmd):
 	intro='                                         _______ __\n.-----.----.-----.----.-----.-----.-----|     __|__|--------.\n|  _  |   _|  _  |  __|  -__|__ --|__ --|__     |  |        |\n|   __|__| |_____|____|_____|_____|_____|_______|__|__|__|__|\n|__|						by panic\n'
 	prompt='pSim>'
+	importlist=['Process from multiprocessing','Cmd from cmd'] #initialize list of imports with default imported modules
+	pdict={} #dictionary of user-defined processes
 
-	#command list
-	def do_import(self,arg): #command to import modules
-		importlist.append(arg)
-		arg=str.split(arg.lower())
-		try:
-			if len(arg)==1:
+	def do_import(self,arg): #command to import modules import arg[0] from arg[2]
+		arg=str.split(arg)
+		if len(arg)==1:
+			try:
 				globals()[arg[0]]=__import__(arg[0])
-			elif len(arg)==3:
-				if arg[1]=='as':
-					globals()[arg[2]]=__import__(arg[0])
-				else:
-					raise
-			else:
-				raise
-		except:
-			print('Error: Invalid syntax.')
-			importlist.pop()
-	
+				t.importlist.append(arg[0])
+			except:
+				print('Error: module ',arg[0],' does not exist')
+		elif len(arg)==3 and arg[1]=='as':
+			try:
+				globals()[arg[2]]=__import__(arg[0])
+				t.importlist.append(' '.join(arg))
+			except:
+				print('Error: module ',arg[0],' does not exist')
+		else:
+			print('Error: invalid syntax')
+		
 	def do_importlist(self,arg): #displays list of imported modules
-		for i in importlist:
+		for i in t.importlist:
 			print(i)
+
+	def do_plist(self,arg): #displays list of user-defined processes
+		for i in t.pdict:
+			print(i)
+			
+	def do_create(self,arg): #command to define a process as an imported function--no support for arguments
+		arg=str.split(arg)
+		if len(arg)==1:
+			t.pdict[arg[0]]=Process(target=eval(arg[0]))
+		elif len(arg)==3 and arg[1]=='as':
+			t.pdict[arg[2]]=Process(target=eval(arg[0]))
+		else:
+			print('Error: invalid syntax')
+	
+	def do_spawn(self,arg): #spawns a new user-defined process
+		if arg in t.pdict:
+			try:
+				t.pdict[arg].start()
+			except:
+				print('Error: process ',arg,' already spawned')
+		else:
+			print('Error: process ',arg,'not created')
+	
+	def do_exec(self,arg): #executes python code during runtime
+		try:
+			exec(arg)
+		except:
+			print('Error:')
 	
 	def do_exit(self,arg): #quits pSim
 		exit()
 
 if __name__=='__main__':
+	set_start_method('spawn',force=True)
 	t().cmdloop()
